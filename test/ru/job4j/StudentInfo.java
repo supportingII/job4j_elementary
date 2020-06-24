24e6cd29094e79f336c175245960b39b31877d15
supportingII <slavikman@bk.ru>
2020-06-24 22:12:38 +0300

    3.3. Разбор первой программы. [#306303]

---
+++ test/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Slava");
+        System.out.println("05.06.1987");
+    }
+}
