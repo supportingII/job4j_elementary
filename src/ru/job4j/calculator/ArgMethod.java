a728124dd10b2485696710ac3eb9d27b93857464
supportingII <slavikman@bk.ru>
2020-06-25 23:03:08 +0300

    4.1. Аргументы [#306294]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+    }
+
+}
