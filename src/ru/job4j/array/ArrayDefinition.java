b07703eab46f7db385289812add690413d73f91d
supportingII <slavikman@bk.ru>
2020-07-13 17:33:47 +0300

    6.0.1. Объявление массива. [#306279]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] firstArray = new short[10];
+        String[] secondArray = new String[10];
+        float[] lastArray = new float[40];
+    }
+}
